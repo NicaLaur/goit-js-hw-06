191a2639dd0563f11af3f7d981d9a35e9fe7313b
NicaLaur <142085072+NicaLaur@users.noreply.github.com>
2024-01-10 15:26:19 +0200

    Update script6.js

--- 6/script6.js
+++ 6/script6.js
@@ -3,14 +3,14 @@ const inputText = document.querySelector("#validation-input");
 inputText.addEventListener('blur', function () {
     
     const textValue = inputText.value.trim();
-    const minLength = inputText.getAttribute('data-length')
+    const correctLength = inputText.getAttribute('data-length')
     
-    if(textValue.length < minLength) {
-      inputText.classList.remove('valid');
-        inputText.classList.add('invalid');
-    } else {
+    if(textValue.length == correctLength) {
         inputText.classList.remove('invalid');
         inputText.classList.add('valid');
+    } else {
+        inputText.classList.remove('valid');
+        inputText.classList.add('invalid');
     }
 
 })
